29a4f0bb6dfc381132f3ddab8662e534f891b70d
Gabriella Hevesy <55456609+napoligab@users.noreply.github.com>
2022-08-25 17:32:53 +0100

    model corrections

--- models/User.model.js
+++ models/User.model.js
@@ -1,43 +1,47 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
-  { firstName: { 
-      type: String, 
+  {
+    firstName: {
+      type: String,
       required: true,
-      trim: true
+      trim: true,
     },
-    lastName: { 
-      type: String, 
+    lastName: {
+      type: String,
       required: true,
-      trim: true
+      trim: true,
     },
-    email: { 
-      type: String, 
-      required: true, 
+    email: {
+      type: String,
+      required: true,
       unique: true,
-      trim: true
+      trim: true,
     },
-    password: { 
-      type: String, 
-      required: true },
-    city: { 
-      type: String, 
-      required: true},
-	  fundedConcerts: [{ type: Schema.Types.ObjectId, ref:'Concert'}],
-    creditCard:{ 
-      type: Number, 
-      required: true
+    password: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    fundedConcerts: [{ type: Schema.Types.ObjectId, ref: 'Concert' }],
+    creditCard: {
+      type: Number,
+      required: true,
     },
     profilePicture: String,
     admin: {
       type: Boolean,
-      default: false
+      default: false,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-const User = model("User", userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
